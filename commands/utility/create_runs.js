d00253298955d3a1eb124de75efdc737e6e7a7fe
Aaron Ofengender <aofengen@gmail.com>
2024-10-04 10:33:19 -0400

    data pulled and message created

--- commands/utility/create_runs.js
+++ commands/utility/create_runs.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, GuildForumThreadManager, ThreadAutoArchiveDuration } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,35 +19,93 @@ module.exports = {
         
         for(let i = 0; i < obj.count; i++) {
             let newObj = new Object();
+            let currentRun = obj.results[i];
+            
+            const forum = '1277184196745363486';
+            if (currentRun.event.id === eventID && currentRun.runners[0].name != 'Staff') {
+                let runnerNames = [];
+                let runnerPronouns = [];
 
-            if (obj.results[i].event.id === eventID && obj.results[i].runners[0].name != 'Staff') {
-                // console.log(obj.results[i]);
-                newObj.name = obj.results[i].name;
-                newObj.estimate = obj.results[i].run_time;
-                newObj.host = obj.results[i].hosts[0].name;
+                let commentatorNames = [];
+                let commentatorPronouns = [];
 
-                if (obj.results[i].runners.length == 1) {
-                    newObj.runner = obj.results[i].runners[0].name;
+                // console.log(currentRun);
+                newObj.name = currentRun.name;
+                newObj.category = currentRun.category;
+                newObj.console = currentRun.console;
+                newObj.estimate = currentRun.run_time;
+                newObj.host = currentRun.hosts[0].name;
+                newObj.hostPronouns = currentRun.hosts[0].pronouns != "" ? currentRun.hosts[0].pronouns : "No Pronouns";
+
+                if (currentRun.runners.length == 1) {
+                    newObj.runners = currentRun.runners[0].name;
+                    newObj.runnerPronouns = currentRun.runners[0].pronouns != "" ? currentRun.runners[0].pronouns : "No Pronouns";
                 } else {
-                    let multipleRunners = "";
-                    for (let j = 0; j < obj.results[i].runners.length; j++) {
-                        multipleRunners += obj.results[i].runners[j].name + ", ";
+                    for (let j = 0; j < currentRun.runners.length; j++) {
+                        runnerNames.push(currentRun.runners[j].name);
+                        runnerPronouns.push(currentRun.runners[j].pronouns != "" ? currentRun.runners[j].pronouns : "No Pronouns");
                     }
-                    newObj.runner = multipleRunners
+                    newObj.runners = runnerNames;
+                    newObj.runnerPronouns = runnerPronouns
                 } 
 
-                if (obj.results[i].commentators.length == 0) {
-                    newObj.commentators = "No Commentators";
-                } else if (obj.results[i].commentators.length == 1) {
-                    newObj.commentators = obj.results[i].commentators[0].name;
+                if (currentRun.commentators.length == 0) {
+                    newObj.commentators = "None";
+                    newObj.commentatorPronouns = "";
+                } else if (currentRun.commentators.length == 1) {
+                    newObj.commentators = currentRun.commentators[0].name;
+                    newObj.commentatorPronouns = currentRun.commentators[0].pronouns != "" ? currentRun.commentators[0].pronouns : "No Pronouns";
                 } else {
-                    let multipleCommentators = "";
-                    for (let j = 0; j < obj.results[i].commentators.length; j++) {
-                        multipleCommentators += obj.results[i].commentators[j].name + ", ";
+                    for (let j = 0; j < currentRun.commentators.length; j++) {
+                        commentatorNames.push(currentRun.commentators[j].name);
+                        commentatorPronouns.push(currentRun.commentators[j].pronouns != "" ? currentRun.commentators[j].pronouns : "No Pronouns")
                     }
-                    newObj.commentators = multipleCommentators
+                    newObj.commentators = commentatorNames;
+                    newObj.commentatorPronouns = commentatorPronouns;
                 } 
                 // console.log(newObj);
+
+                let runners = "";
+                let comms = "";
+
+                if (!Array.isArray(newObj.runners)) {
+                    runners = `@${newObj.runners} \(${newObj.runnerPronouns})`
+                } else {
+                    for (let i = 0; i < newObj.runners.length; i++) {
+                        runners += `@${newObj.runners[i]} \(${newObj.runnerPronouns[i]}), `
+                    }
+                }
+
+                if (!Array.isArray(newObj.commentators)) {
+                    if (newObj.commentators == "None") {
+                        comms = `${newObj.commentators}`;
+                    } else {
+                        comms = `@${newObj.commentators} \(${newObj.commentatorPronouns})`
+                    }
+                } else {
+                    for (let i = 0; i < newObj.commentators.length; i++) {
+                        comms += `@${newObj.commentators[i]} \(${newObj.commentatorPronouns[i]}), `
+                    }
+                }
+
+                const newMessage = `Game: ${newObj.name}\nCategory: ${newObj.category}\nPlatform: ${newObj.console}\nRun Estimate: ${newObj.estimate}\n` + 
+                                   `Runners: ${runners}\nCommentators: ${comms}\nHost: @${newObj.host} \(${newObj.hostPronouns}\)`.trimStart();
+
+                
+                console.log(`\nRun Info for ${newObj.name}:`)
+                console.log(newMessage);
+                
+                // const thread = await forum.threads
+                //    .create({
+                //         name: newObj.name,
+                //         autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+                //         message: {
+                //             content: newMessage
+                //         },
+                //         reason: `Run information/discussion for ${newObj.name}`
+                //    })
+                //    .then(console.log(`Created thread: ${thread.name}`))
+                //    .error(console.error);
             } else {
                 continue;
             }
